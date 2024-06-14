b00204f1e12476b4431cec1616d9f1fc8f2cd421
asdqdsa <asdqdsa@protonmail.com>
2024-06-14 20:15:40 +0400

    feat: add form state

--- src/components/arrow-button/ArrowButton.tsx
+++ src/components/arrow-button/ArrowButton.tsx
@@ -1,19 +1,44 @@
-import arrow from 'src/images/arrow.svg';
+import arrow from "src/images/arrow.svg";
 
-import styles from './ArrowButton.module.scss';
+import styles from "./ArrowButton.module.scss";
+import React, { useState } from "react";
 
 /** Функция для обработки открытия/закрытия формы */
 export type OnClick = () => void;
+type Props = {
+	state?: boolean;
+	onCLick?: () => void;
+};
+export const ArrowButton = (props: Props) => {
+	// const [isMenuOpen, setisMenuOpen] = useState(false);
 
-export const ArrowButton = () => {
+	// const handleMenuState: OnClick = () => {
+	// 	setisMenuOpen(!isMenuOpen);
+	// 	console.log(isMenuOpen);
+	// };
+	console.log(props);
 	return (
 		/* Не забываем указаывать role и aria-label атрибуты для интерактивных элементов */
 		<div
-			role='button'
-			aria-label='Открыть/Закрыть форму параметров статьи'
+			onClick={props.onCLick}
+			onKeyDown={props.onCLick}
+			role="button"
+			aria-label="Открыть/Закрыть форму параметров статьи"
 			tabIndex={0}
-			className={styles.container}>
-			<img src={arrow} alt='иконка стрелочки' className={styles.arrow} />
+			className={props.state ? `${styles.contaner}` : `${styles.contaner} `}
+			// className={styles.container}
+		>
+			<img
+				src={arrow}
+				alt="иконка стрелочки"
+				// `${styles.container} ${styles.container_open}`
+				// className={
+				// 	props.state
+				// 		? `${styles.arrow}`
+				// 		: `${styles.arrow} ${styles.arrow_open}`
+				// }
+				className={styles.arrow}
+			/>
 		</div>
 	);
 };
--- src/components/article-params-form/ArticleParamsForm.tsx
+++ src/components/article-params-form/ArticleParamsForm.tsx
@@ -1,17 +1,127 @@
-import { ArrowButton } from 'components/arrow-button';
-import { Button } from 'components/button';
+import { ArrowButton } from "components/arrow-button";
+import { Button } from "components/button";
 
-import styles from './ArticleParamsForm.module.scss';
+import styles from "./ArticleParamsForm.module.scss";
+import { RadioGroup } from "../radio-group";
+import { Children, useState } from "react";
+import { Separator } from "../separator";
+import { Select } from "../select";
+import { OnClick } from "../arrow-button/ArrowButton";
 
 export const ArticleParamsForm = () => {
+	const [isMenuOpen, setisMenuOpen] = useState(false);
+	console.log(isMenuOpen);
+	const handleMenuState: OnClick = () => {
+		setisMenuOpen(!isMenuOpen);
+		// console.log(isMenuOpen);
+	};
 	return (
 		<>
-			<ArrowButton />
-			<aside className={styles.container}>
+			<ArrowButton state={isMenuOpen} onCLick={handleMenuState} />
+			<aside
+				className={
+					isMenuOpen
+						? `${styles.container} ${styles.container_open}`
+						: `${styles.container}`
+				}
+			>
 				<form className={styles.form}>
+					{/* <Select
+						selected={null}
+						options={[
+							{
+								title: "aaaa",
+								value: "aaa",
+								className: "sss",
+								optionClassName: "sdf",
+							},
+							{
+								title: "bbbb",
+								value: "bbb",
+								className: "sss",
+								optionClassName: "sdf",
+							},
+						]}
+					/> */}
+					{/* <RadioGroup
+						name="Name"
+						selected={{
+							title: "dddd",
+							value: "ddd",
+							className: "sss",
+							optionClassName: "sdf",
+						}}
+						title="Titel"
+						options={[
+							{
+								title: "gggg",
+								value: "ggg",
+								className: "sss",
+								optionClassName: "sdf",
+							},
+							{
+								title: "ffff",
+								value: "fff",
+								className: "sss",
+								optionClassName: "sdf",
+							},
+						]}
+					></RadioGroup> */}
+					{/* <Select
+						selected={null}
+						options={[
+							{
+								title: "qqqq",
+								value: "qqq",
+								className: "sss",
+								optionClassName: "sdf",
+							},
+							{
+								title: "rrrr",
+								value: "rrr",
+								className: "sss",
+								optionClassName: "sdf",
+							},
+						]}
+					/>
+					<Separator />
+					<Select
+						selected={null}
+						options={[
+							{
+								title: "fds",
+								value: "fds",
+								className: "sss",
+								optionClassName: "sdf",
+							},
+							{
+								title: "fdss",
+								value: "fds",
+								className: "sss",
+								optionClassName: "sdf",
+							},
+						]}
+					/>
+					<Select
+						selected={null}
+						options={[
+							{
+								title: "fds",
+								value: "fds",
+								className: "sss",
+								optionClassName: "sdf",
+							},
+							{
+								title: "fdss",
+								value: "fds",
+								className: "sss",
+								optionClassName: "sdf",
+							},
+						]}
+					/> */}
 					<div className={styles.bottomContainer}>
-						<Button title='Сбросить' type='reset' />
-						<Button title='Применить' type='submit' />
+						<Button title="Сбросить" type="reset" />
+						<Button title="Применить" type="submit" />
 					</div>
 				</form>
 			</aside>
