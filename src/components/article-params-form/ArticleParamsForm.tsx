42975d986f3c8df5e1871450a7afcb48cf474db9
asdqdsa <asdqdsa@protonmail.com>
2024-06-19 23:18:59 +0400

    fix: style form components
     feat: add reset button state

--- src/components/article-params-form/ArticleParamsForm.tsx
+++ src/components/article-params-form/ArticleParamsForm.tsx
@@ -69,6 +69,19 @@ export const ArticleParamsForm = (props: ArticleParamsFormProps) => {
 		});
 	};
 
+	const handleFormReset = (e: SyntheticEvent) => {
+		e.preventDefault();
+		console.log("hehe", { fontFamilyOption: fontFamily });
+		props.setArticleState({
+			...props.articleState,
+			fontFamilyOption: defaultArticleState.fontFamilyOption,
+			fontColor: defaultArticleState.fontColor,
+			fontSizeOption: defaultArticleState.fontSizeOption,
+			contentWidth: defaultArticleState.contentWidth,
+			backgroundColor: defaultArticleState.backgroundColor,
+		});
+	};
+
 	useOutsideClickClose({
 		isOpen: isOpenMenu,
 		rootRef,
@@ -86,7 +99,28 @@ export const ArticleParamsForm = (props: ArticleParamsFormProps) => {
 						: clsx(styles.container)
 				}
 			>
-				<form onSubmit={handleFormSubmit} className={styles.form}>
+				<form
+					onSubmit={handleFormSubmit}
+					onReset={handleFormReset}
+					className={styles.form}
+					title="Title"
+					name="TTT"
+				>
+					<div>
+						<h2
+							style={{
+								display: "inline-block",
+								textAlign: "left",
+								fontSize: 31,
+								fontFamily: "Open Sans, sans-serif",
+								fontWeight: 800,
+								marginBlockEnd: 50,
+							}}
+						>
+							ЗАДАЙТЕ ПАРАМЕТРЫ
+						</h2>
+					</div>
+
 					<Select
 						selected={fontFamily}
 						options={fontFamilyOptions}
@@ -123,88 +157,8 @@ export const ArticleParamsForm = (props: ArticleParamsFormProps) => {
 						onChange={setContentWidth}
 						title="Width"
 					/>
-					{/* <RadioGroup
-						name="Name"
-						selected={{
-							title: "dddd",
-							value: "ddd",
-							className: "sss",
-							optionClassName: "sdf",
-						}}
-						title="Titel"
-						options={[
-							{
-								title: "gggg",
-								value: "ggg",
-								className: "sss",
-								optionClassName: "sdf",
-							},
-							{
-								title: "ffff",
-								value: "fff",
-								className: "sss",
-								optionClassName: "sdf",
-							},
-						]}
-					></RadioGroup> */}
-					{/* <Select
-						selected={null}
-						options={[
-							{
-								title: "qqqq",
-								value: "qqq",
-								className: "sss",
-								optionClassName: "sdf",
-							},
-							{
-								title: "rrrr",
-								value: "rrr",
-								className: "sss",
-								optionClassName: "sdf",
-							},
-						]}
-					/>
-					<Separator />
-					<Select
-						selected={null}
-						options={[
-							{
-								title: "fds",
-								value: "fds",
-								className: "sss",
-								optionClassName: "sdf",
-							},
-							{
-								title: "fdss",
-								value: "fds",
-								className: "sss",
-								optionClassName: "sdf",
-							},
-						]}
-					/>
-					<Select
-						selected={null}
-						options={[
-							{
-								title: "fds",
-								value: "fds",
-								className: "sss",
-								optionClassName: "sdf",
-							},
-							{
-								title: "fdss",
-								value: "fds",
-								className: "sss",
-								optionClassName: "sdf",
-							},
-						]}
-					/> */}
 					<div className={styles.bottomContainer}>
-						<Button
-							// onClick={}
-							title="Сбросить"
-							type="reset"
-						/>
+						<Button title="Сбросить" type="reset" />
 						<Button title="Применить" type="submit" />
 					</div>
 				</form>
